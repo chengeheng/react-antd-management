9086b5bb09bc294a15dc805bffb65603d6ab94ef
chengeheng <465819676@qq.com>
2022-06-23 14:17:47 +0800

    init typescript environment

--- prebuild.js
+++
@@ -1,8 +0,0 @@
-const color = require("./scripts/color.js");
-color()
-  .then((less) => {
-    console.log("Theme generated successfully");
-  })
-  .catch((error) => {
-    console.log("Error", error);
-  });
